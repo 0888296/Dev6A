05fd06decf636f092cc1593bf33bbd170a1f2ab9
0888296 <5314zain@gmail.com>
2015-12-15 16:41:15 +0100

    commit
    assignment 1 af

---
+++ INFDEV02-6A - Practical assignment/EntryPoint/Program.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EntryPoint
+{
+#if WINDOWS || LINUX
+  public static class Program
+  {
+    [STAThread]
+    static void Main()
+    {
+      var fullscreen = false;
+      read_input:
+      switch (Microsoft.VisualBasic.Interaction.InputBox("Which assignment shall run next? (1, 2, 3, 4, or q for quit)", "Choose assignment", VirtualCity.GetInitialValue()))
+      {
+        case "1":
+          using (var game = VirtualCity.RunAssignment1(SortSpecialBuildingsByDistance, fullscreen))
+            game.Run();
+          break;
+        case "2":
+          using (var game = VirtualCity.RunAssignment2(FindSpecialBuildingsWithinDistanceFromHouse, fullscreen))
+            game.Run();
+          break;
+        case "3":
+          using (var game = VirtualCity.RunAssignment3(FindRoute, fullscreen))
+            game.Run();
+          break;
+        case "4":
+          using (var game = VirtualCity.RunAssignment4(FindRoutesToAll, fullscreen))
+            game.Run();
+          break;
+        case "q":
+          return;
+      }
+      goto read_input;
+    }
+
+    private static IEnumerable<Vector2> SortSpecialBuildingsByDistance(Vector2 house, IEnumerable<Vector2> specialBuildings)
+    {
+            int[] houseDistance = new int[50];
+            int[,] totalDistance = new int[50,3]; 
+            int home = 0;
+            int o = 0;
+
+            foreach (var value in specialBuildings)
+            {
+                float buildingx = value.X;
+                float buildingy = value.Y;
+                float homex = house.X;
+                float homey = house.Y;
+
+                float difx = homex - buildingx;
+                float dify = homey - buildingy;
+
+                int calcx = (int)Math.Ceiling(difx);
+                int calcy = (int)Math.Ceiling(dify);
+
+                int calctotal = (calcx * calcx) + (calcy * calcy);
+                houseDistance[home] = calctotal;
+                totalDistance[home, 0] = calctotal;
+                totalDistance[home, 1] = (int)Math.Ceiling(buildingx);
+                totalDistance[home, 2] = (int)Math.Ceiling(buildingy);
+                home++;
+            }
+    
+            MergeSort_Recursive(houseDistance, 0, houseDistance.Length - 1);
+
+            int[,] totalHouseDistance = new int[50, 2];
+            List<Vector2> distance = new List<Vector2>();
+            int i = 0;
+            int j = 0;
+            int k = 0;
+                
+            while(i < houseDistance.Length)
+            {
+                if (totalDistance[j,0] == houseDistance[i])
+                {
+                    totalHouseDistance[i, 0] = totalDistance[j, 1];
+                    totalHouseDistance[i, 1] = totalDistance[j, 2];
+                    i++;
+                    j = 0;
+                }
+                else
+                {
+                    j++;
+                }
+            }
+
+            while (k < 50)
+            {
+                distance.Add(new Vector2(totalHouseDistance[k,0],totalHouseDistance[k,1]));
+                k++;
+            }
+            return distance;
+        }
+
+        static public void DoMerge(int[] numbers, int left, int mid, int right)
+        {
+            int[] temp = new int[50];
+            int i, left_end, num_elements, tmp_pos;
+
+            left_end = (mid - 1);
+            tmp_pos = left;
+            num_elements = (right - left + 1);
+
+            while ((left <= left_end) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid])
+                    temp[tmp_pos++] = numbers[left++];
+                else
+                    temp[tmp_pos++] = numbers[mid++];
+            }
+
+            while (left <= left_end)
+                temp[tmp_pos++] = numbers[left++];
+
+            while (mid <= right)
+                temp[tmp_pos++] = numbers[mid++];
+
+            for (i = 0; i < num_elements; i++)
+            {
+                numbers[right] = temp[right];
+                right--;
+            }
+        }
+
+        static public void MergeSort_Recursive(int[] numbers, int left, int right)
+        {
+            int mid;
+
+            if (right > left)
+            {
+                mid = (right + left) / 2;
+                MergeSort_Recursive(numbers, left, mid);
+                MergeSort_Recursive(numbers, (mid + 1), right);
+
+                DoMerge(numbers, left, (mid + 1), right);
+            }
+        }
+
+        private static IEnumerable<IEnumerable<Vector2>> FindSpecialBuildingsWithinDistanceFromHouse(
+      IEnumerable<Vector2> specialBuildings, 
+      IEnumerable<Tuple<Vector2, float>> housesAndDistances)
+    {
+      return
+          from h in housesAndDistances
+          select
+            from s in specialBuildings
+            where Vector2.Distance(h.Item1, s) <= h.Item2
+            select s;
+    }
+
+    private static IEnumerable<Tuple<Vector2, Vector2>> FindRoute(Vector2 startingBuilding, 
+      Vector2 destinationBuilding, IEnumerable<Tuple<Vector2, Vector2>> roads)
+    {
+      var startingRoad = roads.Where(x => x.Item1.Equals(startingBuilding)).First();
+      List<Tuple<Vector2, Vector2>> fakeBestPath = new List<Tuple<Vector2, Vector2>>() { startingRoad };
+      var prevRoad = startingRoad;
+      for (int i = 0; i < 30; i++)
+      {
+        prevRoad = (roads.Where(x => x.Item1.Equals(prevRoad.Item2)).OrderBy(x => Vector2.Distance(x.Item2, destinationBuilding)).First());
+        fakeBestPath.Add(prevRoad);
+      }
+      return fakeBestPath;
+    }
+
+    private static IEnumerable<IEnumerable<Tuple<Vector2, Vector2>>> FindRoutesToAll(Vector2 startingBuilding, 
+      IEnumerable<Vector2> destinationBuildings, IEnumerable<Tuple<Vector2, Vector2>> roads)
+    {
+      List<List<Tuple<Vector2, Vector2>>> result = new List<List<Tuple<Vector2, Vector2>>>();
+      foreach (var d in destinationBuildings)
+      {
+        var startingRoad = roads.Where(x => x.Item1.Equals(startingBuilding)).First();
+        List<Tuple<Vector2, Vector2>> fakeBestPath = new List<Tuple<Vector2, Vector2>>() { startingRoad };
+        var prevRoad = startingRoad;
+        for (int i = 0; i < 30; i++)
+        {
+          prevRoad = (roads.Where(x => x.Item1.Equals(prevRoad.Item2)).OrderBy(x => Vector2.Distance(x.Item2, d)).First());
+          fakeBestPath.Add(prevRoad);
+        }
+        result.Add(fakeBestPath);
+      }
+      return result;
+    }
+  }
+#endif
+}
